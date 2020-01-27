41ebe990d623145f8034ca0e1f1b1d6a244bc178
friskit <friskit.china@gmail.com>
2020-01-27 17:24:27 +0800

    ac 717

---
+++ problems/717/main.v01.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// submit start
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        bool flag = true;
+        for(int i=0; i< bits.size(); ++i){
+            if (flag == true){
+                if(bits[i] == 0 and i == bits.size() - 1){
+                    return true;
+                }else if (bits[i] == 1){
+                    flag = false;
+                }
+            }else{
+                if (bits[i] == 0 and i == bits.size() - 1){
+                    return false;
+                }else if((bits[i] == 0 and i != bits.size() - 1) || bits[i] == 1){
+                    flag = true;
+                }
+            }
+        }
+        return false;
+    }
+};
+// submit end
+
+int main(){
+    Solution solutionObject;
+    vector<int> bits{1,0,0};
+    // vector<int> bits{1,1,1,0};
+    cout<<solutionObject.isOneBitCharacter(bits)<<endl;
+    return 0;
+}
