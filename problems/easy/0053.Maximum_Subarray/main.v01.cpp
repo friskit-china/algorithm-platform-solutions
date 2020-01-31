a6ea4cf2b80c356bcf5099dabf472f804316704e
friskit <friskit.china@gmail.com>
2020-01-31 16:00:49 +0800

    ac 53

---
+++ problems/easy/0053.Maximum_Subarray/main.v01.cpp
@@ -0,0 +1,33 @@
+// https://leetcode-cn.com/problems/maximum-subarray/
+#include <iostream>
+#include <vector>
+#include <limits>
+using namespace std;
+
+// submit start
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum_max = nums[0];
+        int sum_cur = nums[0];
+        for (int i = 1; i < nums.size(); i += 1){
+            if (sum_cur > 0){
+                sum_cur = sum_cur + nums[i];
+            }else{
+                sum_cur = nums[i];
+            }
+            if (sum_cur > sum_max){
+                sum_max = sum_cur;
+            }
+        }
+        return sum_max;
+    }
+};
+// submit end
+
+int main(){
+    Solution sl;
+    vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    cout<<sl.maxSubArray(nums)<<endl;
+    return 0;
+}
