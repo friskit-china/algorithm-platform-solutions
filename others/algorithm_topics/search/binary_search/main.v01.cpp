923b364e7938f27864ab96e30bd8bb828ae3592a
friskit <friskit.china@gmail.com>
2020-02-06 12:26:50 +0800

    update binarysearch

--- others/algorithm_topics/search/binary_search/main.v01.cpp
+++ others/algorithm_topics/search/binary_search/main.v01.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <algorithm>
 using namespace std;
 
 class Algorithms{
     public: 
-        int static binary_search(vector<int> arr, int element){
+        int static binary_search(vector<int> &arr, int element){
             int left = 0;
-            int right = arr.size() - 1;
+            int right = arr.size();
             while (left < right){
                 int mid = (left + right) / 2;
                 if (arr[mid] < element){
-                    left = mid + 1;
+                    left = mid - 1;
                 }else if (arr[mid] > element){
-                    right = mid - 1;
+                    right = mid + 1;
                 }else if (arr[mid] == element){
                     return mid;
                 }
@@ -22,7 +24,33 @@ class Algorithms{
 };
 
 int main(){
-    vector<int> arr = {100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119};
-    cout<<Algorithms::binary_search(arr, 10);
+    int data = -1;
+
+    std::ifstream fin_tiny;
+    fin_tiny.open("data.tinyW.txt");
+    if (!fin_tiny){
+        std::cout<<"I/O error"<<endl;
+        throw "I/O error";
+    }
+    vector<int> * arr_tiny = new vector<int>();
+    while (fin_tiny>>data){
+        arr_tiny->push_back(data);
+    }
+    sort(arr_tiny->begin(), arr_tiny->end());
+
+    std::ifstream fin_large;
+    fin_large.open("data.largeW.txt");
+    if (!fin_large){
+        std::cout<<"I/O error"<<endl;
+        throw "I/O error";
+    }
+    vector<int> * arr_large = new vector<int>();
+    while (fin_large>>data){
+        arr_large->push_back(data);
+    }
+    sort(arr_large->begin(), arr_large->end());
+
+    std::cout<<Algorithms::binary_search(*arr_tiny, 12)<<endl;
+    std::cout<<Algorithms::binary_search(*arr_large, 12)<<endl;
     return 0;
 }
