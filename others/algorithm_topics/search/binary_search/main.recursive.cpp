b378251a2ee04c9faca69d5aed669e5e5146700c
friskit <friskit.china@gmail.com>
2020-02-06 13:07:30 +0800

    update binary search: add recursive version

---
+++ others/algorithm_topics/search/binary_search/main.recursive.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+using namespace std;
+
+class Algorithms{
+    public: 
+        int static binary_search(vector<int> &arr, int element, int left, int right){
+            while (left < right){
+                int mid = (left + right) / 2;
+                if (arr[mid] < element){
+                    return binary_search(arr, element, mid - 1, right);
+                }else if (arr[mid] > element){
+                    return binary_search(arr, element, left, mid + 1);
+                }else if (arr[mid] == element){
+                    return mid;
+                }
+            }
+            return -1;
+        }
+};
+
+int main(){
+    int data = -1;
+
+    std::ifstream fin_tiny;
+    fin_tiny.open("data.tinyW.txt");
+    if (!fin_tiny){
+        std::cout<<"I/O error"<<endl;
+        throw "I/O error";
+    }
+    vector<int> * arr_tiny = new vector<int>();
+    while (fin_tiny>>data){
+        arr_tiny->push_back(data);
+    }
+    sort(arr_tiny->begin(), arr_tiny->end());
+
+    std::ifstream fin_large;
+    fin_large.open("data.largeW.txt");
+    if (!fin_large){
+        std::cout<<"I/O error"<<endl;
+        throw "I/O error";
+    }
+    vector<int> * arr_large = new vector<int>();
+    while (fin_large>>data){
+        arr_large->push_back(data);
+    }
+    sort(arr_large->begin(), arr_large->end());
+
+    std::cout<<Algorithms::binary_search(*arr_tiny, 12, 0, arr_tiny->size() - 1)<<endl;
+    std::cout<<Algorithms::binary_search(*arr_large, 12, 0, arr_large->size() + 1)<<endl;
+    return 0;
+}
